5ca00276a4e58a75b48b51bd129c2ce6fa7dc5b9
User <aned@i.ua>
2017-07-16 13:49:34 +0300

    Create Router, Autoload. Corrected index.php

--- gallery/index.php
+++ gallery/index.php
@@ -1,8 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 16.07.2017
- * Time: 13:13
- */
-echo 'Red';
+
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+session_start();
+
+define('ROOT',dirname(__FILE__));
+require_once (ROOT.'/components/Autoload.php');
+
+
+
+$router = new Router();
+
+$router -> run();
+
+
+
