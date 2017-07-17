b6197dfa16718205d4de510f52712e440b18433c
User <aned@i.ua>
2017-07-17 15:01:50 +0300

    bootstrap

--- gallery/views/layouts/header.php
+++ gallery/views/layouts/header.php
@@ -7,9 +7,9 @@
     <meta name="description" content="#" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Online gallery</title>
-    <link rel="stylesheet" href="/template/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="/template/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/template/style/main.css">
-    <script type="text/javascript" src="/template/bootstrap/js/bootstrap.js"></script>
+    <script type="text/javascript" src="/template/bootstrap/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="/template/script/jquery-3.2.1.min.js"></script>
 
 </head>
